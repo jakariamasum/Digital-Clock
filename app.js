f4e68993f36f1905d782f2e9f8f57531e078e40b
jakariamasum <jmphy698@gmail.com>
2023-01-04 15:08:38 +0600

    digital clock

---
+++ app.js
@@ -0,0 +1,20 @@
+function digitalClock(){
+    let date=new Date(); 
+    let hours=date.getHours(); 
+    let minutes=date.getMinutes(); 
+    let seconds=date.getSeconds(); 
+    let timeFormate='AM';
+    if(hours>12)
+    {
+        hours-=12;
+        timeFormate='PM';
+    }
+    hours=hours<10?'0'+hours:hours;
+    minutes=minutes<10?'0'+minutes:minutes;
+    seconds=seconds<10?'0'+seconds:seconds;
+    let finalTime=`${hours}:${minutes}:${seconds}`;
+    document.getElementById('time').innerText=finalTime;
+    document.querySelector('small').innerText=timeFormate;
+    setInterval(digitalClock,1000);
+}
+digitalClock();
